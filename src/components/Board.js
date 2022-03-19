7d738471ae530dc8f893a198f52fb655d85274bb
PepperMcLean <jackmcl206@gmail.com>
2022-03-19 13:49:32 -0500

    create nav bar

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,6 +1,8 @@
 import React from 'react'
 
 function Board() {
+
+  
   return (
     <div>Board</div>
   )
--- src/index.js
+++ src/index.js
@@ -3,13 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={board}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
